48314672ba900575f75bd84be87d76efa6dfa7ec
Casper <casfro01@easv365.dk>
2025-02-06 12:56:18 +0100

    fxider yeah

--- src/main/java/com/example/ticketgui/Main.java
+++ src/main/java/com/example/ticketgui/Main.java
@@ -11,12 +11,15 @@ import java.util.Objects;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main Window.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainWindow.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("stylesheet.css")).toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        stage.setMaximized(true);
+        MainWindowController mwc = fxmlLoader.getController();
+        mwc.test(stage);
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/ticketgui/MainWindowController.java
+++ src/main/java/com/example/ticketgui/MainWindowController.java
@@ -1,14 +1,45 @@
 package com.example.ticketgui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class MainWindowController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindowController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private AnchorPane dataPane;
+    @FXML
+    private AnchorPane root;
+    @FXML
+    private AnchorPane sideMenu;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void test(Stage primaryStage) {
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setLayoutX(dataPane.getLayoutX());
+        scrollPane.setLayoutY(dataPane.getLayoutY());
+        scrollPane.setContent(dataPane);
+        scrollPane.setPrefViewportHeight(dataPane.getHeight());
+        scrollPane.setPrefViewportWidth(dataPane.getWidth());
+        double length = Stage.getWindows().getFirst().getWidth() - sideMenu.getWidth();
+        scrollPane.setPrefSize(length, dataPane.getHeight());
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        root.getChildren().add(scrollPane);
+    }
+
+    // erhmmm ja... man kunne også bare give hvert pane et fxId og .getChildren() - hilsen Casper -> but also made by Casper... træls
+    public void initializeComponents(){
+
     }
 }
