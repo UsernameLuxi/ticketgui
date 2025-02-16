1e6e078bffbbad0bd44e7dca9a72e6f9a84b97d9
Casper <casfro01@easv365.dk>
2025-02-16 19:50:03 +0100

    kommentar

--- src/main/java/com/example/ticketgui/IController.java
+++ src/main/java/com/example/ticketgui/IController.java
@@ -2,6 +2,7 @@ package com.example.ticketgui;
 
 public interface IController {
 
+    // TODO : overvej om dette skal laves til en abstrakt klasse
     void initializeComponents(double width, double height);
     void resizeItems(double width, double height);
 }
